f2963b7168606010ea6844eb884325503eaa1ffd
drewheasman <drewheasman@gmail.com>
2023-07-16 09:47:04 +0100

    Terraform deploy

--- .tf/_providers.tf
+++ .tf/_providers.tf
@@ -1,3 +1,12 @@
+# Following variables as github secrets:
+# AWS_REGION
+# TF_AWS_BUCKET
+# TF_AWS_BUCKET_KEY
+# TF_AWS_ROLE
+
+# Initialise locally with:
+# terraform init -backend-config="bucket=TFSTATE_BUCKET" -backend-config="key=TFSTATE_KEY" -backend-config="region=REGION" -backend-config="profile=PROFILE"
+
 terraform {
   required_version = "~> 1.5"
 
@@ -8,16 +17,10 @@ terraform {
     }
   }
 
-  backend "s3" {
-    bucket = "drewheasman-terraform"
-    key    = "create-barcodes/prod/terraform.tfstate"
-    region = "eu-west-2"
-  }
+  backend "s3" {}
 }
 
-provider "aws" {
-  region = "eu-west-2"
-}
+provider "aws" {}
 
 provider "aws" {
   alias  = "acm_provider"
