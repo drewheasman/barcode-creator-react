1638c6c1486801485af52fb25d2be1de1c305186
drewheasman <drewheasman@gmail.com>
2023-07-20 22:58:12 +0100

    Optimise site

--- .tf/5_cloudfront_distribution.tf
+++ .tf/5_cloudfront_distribution.tf
@@ -19,23 +19,11 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   }
 
   default_cache_behavior {
-    allowed_methods  = ["GET", "HEAD"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = var.bucket_name
-
-    forwarded_values {
-      query_string = false
-
-      cookies {
-        forward = "none"
-      }
-    }
-
+    cache_policy_id        = "658327ea-f89d-4fab-a63d-7e88639e58f6"
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+    target_origin_id       = var.bucket_name
     viewer_protocol_policy = "redirect-to-https"
-    min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
-    compress               = true
   }
 
   restrictions {
