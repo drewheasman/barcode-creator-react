63ab5caa110e4c87980b0b2384a754dcdf0dcfae
drewheasman <drewheasman@gmail.com>
2023-09-05 23:19:45 +0100

    LevelColor test

---
+++ app/src/util/Messages/LevelColor.test.ts
@@ -0,0 +1,16 @@
+import { MessageLevel } from "../../enum/MessageLevel";
+import { levelColor } from "./LevelColor";
+
+describe("levelColor function", () => {
+  it("should return 'danger' for MessageLevel.Error", () => {
+    expect(levelColor(MessageLevel.Error)).toBe("danger");
+  });
+
+  it("should return 'success' for MessageLevel.Info", () => {
+    expect(levelColor(MessageLevel.Info)).toBe("success");
+  });
+
+  it("should return 'warning' for MessageLevel.Warn", () => {
+    expect(levelColor(MessageLevel.Warn)).toBe("warning");
+  });
+});
